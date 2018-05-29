ae47f115be582660978a8ef225056f4beb741f19
jamesfowler <JamesFowlerTX@yahoo.com>
2018-05-29 14:10:11 -0500

    Java Exercises

--- src/Practice.java
+++ src/Practice.java
@@ -1,2 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+
 public class Practice {
-}
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+//            Scanner scanner =new Scanner(System.in);
+//            int N = scanner.nextInt();
+//            if (N>=2 || N<=20){
+//                for(int i=1; i<=10;i++) {
+//                    System.out.println(N + " x " + i +" = " + N * i);
+//                }
+//            }
+//            scanner.close();
+//            System.out.println("================================");
+//            for(int i=0;i<3;i++)
+//            {
+//                String s1=sc.next();
+//                int x=sc.nextInt();
+//                //Complete this line
+//                //s is for String, n is for new line, d is for double. Must specify varialbes afterwards
+//                System.out.printf( "%-15s%03d %n", s1, x);
+//            }
+//            System.out.println("================================");
+        }
+    }
+
